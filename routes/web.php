ff3d652f907dbf219a46c325ed5553650e02553d
rozaq <rzqmrf@gmail.com>
2026-03-05 09:37:50 +0700

    ulang lagi

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProfilController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('profil', ProfilController::class);
+Route::get('/about', function () {
+    return "halaman about";
+});
