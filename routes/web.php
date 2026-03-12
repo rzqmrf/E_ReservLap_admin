565e016d40624b509f30b18bed0937c510ee7ba9
rozaq <rzqmrf@gmail.com>
2026-03-12 08:20:44 +0700

    update tampilan css

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route untuk halaman welcome
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Route untuk halaman home
 Route::get('/', function () {
     return view('home');
 });
@@ -15,6 +17,7 @@ Route::get('/about', function () {
     return view('about');
 });
 
+// Route untuk halaman contact
 Route::get('/contact', function () {
     return view('contact');
 });
